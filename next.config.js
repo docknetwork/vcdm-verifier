9c4f01da844ba0c741d987494f54d7a064e1207c
Sam Hellawell <sshellawell@gmail.com>
2020-05-11 21:32:38 +0100

    remove issuing-verification-app prefix in nextjs build
    Signed-off-by: Sam Hellawell <sshellawell@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,9 +1,2 @@
 const withImages = require('next-images');
-const debug = process.env.NODE_ENV !== "production";
-
-module.exports = withImages({
-  assetPrefix: debug ? '' : '/issuing-verification-app/',
-  experimental: {
-    basePath: debug ? '' : '/issuing-verification-app',
-  },
-});
+module.exports = withImages({});
