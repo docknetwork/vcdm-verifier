37f280f7b3ea580dd917d6ec31918f749ac14729
mike-parkhill <mike-parkhill@users.noreply.github.com>
2021-09-21 21:47:26 +0000

    Deploying to gh-pages from  @ affeb94b1e5443691da25b46750b1858a9baa3d1 🚀

---
+++ _next/static/z_uYG2CP3A-_Ss4yKEp5x/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/337-bf9d19dab2218099f291.js","static/chunks/pages/index-7bd18e74fa8a7591908c.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
